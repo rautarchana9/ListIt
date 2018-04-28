efa9ded5aedb32eff4e2bf43e9b8c2c0f1654940
Archana Raut <rautarchana9@gmail.com>
2018-04-28 22:41:29 +0530

    First version of ListIt App - all features implemented, except setting a background task

--- listit/admin.py
+++ listit/admin.py
@@ -1,7 +1,47 @@
 from django.contrib import admin
+from datetime import date, timedelta
 
 # Register your models here.
 from .models import List, Task, Subtask
+from django.utils.translation import gettext_lazy as _
+
+class ItemInline(admin.StackedInline):
+  model = Subtask
+  extra = 3
+class CustomDueDateFilter(admin.SimpleListFilter):
+  title = ('Due date')
+
+  parameter_name = 'due_date'
+
+  def lookups(self, request, model_admin):
+    return (('A',  _('Today')),
+            ('B',_('This week')),
+            ('C',_('Next week')),
+            ('D', _('Overdue')),
+            )
+         
+  def queryset(self, request, queryset):
+    t = date.today()
+    if self.value() == 'A':
+      return queryset.filter(due_date=t)
+    if self.value() == 'B':
+      return queryset.filter(due_date__gte=t, due_date__lte=t+timedelta(days=8))
+    if self.value() == 'C':
+      return queryset.filter(due_date__gte=t+timedelta(days=8), due_date__lte=t+timedelta(days=15))
+    if self.value() == 'D':
+      return queryset.filter(due_date__lt=t+timedelta(days=-1))
+
+class TaskAdmin(admin.ModelAdmin):
+  fieldsets = [
+        (None,               {'fields': ['title']}),
+        ('Date information', {'fields': ['due_date']}),
+        ('status', {'fields': ['status']})
+    ]
+
+  list_display = ('title', 'due_date', 'status')
+  list_filter = [CustomDueDateFilter]
+  search_fields = ['title']
+  inlines = [ItemInline]
+admin.site.register(Task, TaskAdmin)
 admin.site.register(List)
-admin.site.register(Task)
-admin.site.register(Subtask)
+
--- listit/models.py
+++ listit/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.conf import settings
-
 import datetime
+from model_utils.fields import StatusField
+from model_utils import Choices
+from djchoices import DjangoChoices, ChoiceItem
 
 # Create your models here.
 
@@ -16,21 +19,66 @@ class List(models.Model):
   class Meta:
     ordering = ['title']
 
-class Task(models.Model):
-  title = models.CharField(max_length=60)
-  body = models.TextField()
-  created_at = models.DateField(default=timezone.now, blank=True, null=True)
-  due_date = models.DateField(blank=True, null=True)
-  completed = models.BooleanField(default=False)
-  completed_at = models.DateField(blank=True, null=True)
-  todo_list = models.ForeignKey(List, on_delete=models.CASCADE, null=True)
 
+class SoftDeletionQuerySet(QuerySet):
+  def delete(self):
+      return super(SoftDeletionQuerySet, self).update(deleted_at=timezone.now())
+
+  def hard_delete(self):
+      return super(SoftDeletionQuerySet, self).delete()
+
+  def alive(self):
+      return self.filter(deleted_at=None)
+
+  def dead(self):
+      return self.exclude(deleted_at=None)
+
+class SoftDeletionManager(models.Manager):
+    def __init__(self, *args, **kwargs):
+        self.alive_only = kwargs.pop('alive_only', True)
+        super(SoftDeletionManager, self).__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        if self.alive_only:
+            return SoftDeletionQuerySet(self.model).filter(deleted_at=None)
+        return SoftDeletionQuerySet(self.model)
+
+    def hard_delete(self):
+        return self.get_queryset().hard_delete()
+
+class SoftDeletionModel(models.Model):
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    objects = SoftDeletionManager()
+    all_objects = SoftDeletionManager(alive_only=False)
+
+    class Meta:
+        abstract = True
+
+    def delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def hard_delete(self):
+        super(SoftDeletionModel, self).delete()
+
+class Task(SoftDeletionModel):
+
+  objects = SoftDeletionManager()
+
+  STATUS_TYPE = (
+        ("C", "Completed"),
+        ("P", "Pending"),
+        )
+  title = models.CharField(max_length=60)
+  due_date = models.DateField(null=True, blank=True)
+  todo_list = models.ForeignKey(List, on_delete=models.CASCADE, null=True)
+  status = models.CharField(max_length=1, choices=STATUS_TYPE, null=True)
   def __str__(self):
-        return '%s %s' % (self.title, self.body)
+        return ' %s' % (self.title)
   
   class Meta:
-    get_latest_by = ['due_date']
-
+    ordering = ['due_date']
 
 class Subtask(models.Model):
   title = models.CharField(max_length=200)

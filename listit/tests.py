fff65cbefa96b02e1e49f86315b10d0260635339
Archana Raut <rautarchana9@gmail.com>
2018-05-11 13:17:04 +0530

    Added test for task creation on ListIt

--- listit/tests.py
+++ listit/tests.py
@@ -1,3 +1,21 @@
+from tastypie.test import TestApiClient
+import json
+from datetime import date, timedelta
 from django.test import TestCase
 
-# Create your tests here.
+class ListItTestCase(TestCase):
+  def setUp(self):
+      self.client = TestApiClient()
+
+  def test_add_tasks(self):
+      due_date = date.today() - timedelta(days=2)
+      for i in range(16):
+
+          response = self.client.post('/api/v1/tasks/', data={
+              'title': 'test task',
+              'due_date': due_date,
+              'status': 'Pending'
+              }
+          )
+          due_date = due_date + timedelta(days=1)
+          self.assertEqual(response.status_code, 201)
